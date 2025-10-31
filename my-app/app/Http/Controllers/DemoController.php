993b42d802337e230f8d1dca30df6ac2ee4226e8
nmsaikat <nmsaikat.cf@gmail.com>
2025-10-31 12:25:51 +0600

    Working With App 2 Laravel Response Type

--- my-app/app/Http/Controllers/DemoController.php
+++ my-app/app/Http/Controllers/DemoController.php
@@ -6,5 +6,44 @@ use Illuminate\Http\Request;
 
 class DemoController extends Controller
 {
-    //
+    // Laravel Response Function
+    function demo1(){
+        // return "Hello";
+
+        // return 150;
+
+        // return null;
+
+        // return true;
+
+        // return ["Saikat", "Arafat", "Rafi"];
+
+        // return (
+        //     ["name" => "Saikat", "age" => 24, "city" => "Dhaka", "country" => "Bangladesh"] +
+        //     ["name" => "Arafat", "age" => 25, "city" => "Chittagong", "country" => "Bangladesh"]
+        // );
+
+        // return response()->json(
+        //     ["name" => "Yesmin", "age" => 24, "city" => "Dhaka", "country" => "Bangladesh"]
+        // );
+
+        // return response()->json(
+        //     ["name" => "Yesmin", "age" => 24, "city" => "Dhaka", "country" => "Bangladesh"],
+        //     203
+        // );
+
+        // return redirect('/');
+
+        // return response()->file("favicon.ico");
+
+        // return response()->download("index.php");
+
+        return view('demo');
+
+    }
+
+    // Laravel Request Function 
+    function demo2(){
+        return "Hello";
+    }
 }
--- my-app/routes/web.php
+++ my-app/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DemoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Laravel Response Route
+Route::get('/demo1', [DemoController::class, 'demo1']);
+
+// Laravel Request Route
+Route::get('/demo2', [DemoController::class, 'demo2']);
