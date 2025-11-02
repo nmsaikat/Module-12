5acb9a6027493f023bda80180e4107ae9a33cb02
nmsaikat <nmsaikat.cf@gmail.com>
2025-11-02 08:47:57 +0600

    Cookie In Laravel

--- r-app/app/Http/Controllers/DemoController.php
+++ r-app/app/Http/Controllers/DemoController.php
@@ -7,10 +7,6 @@ use Illuminate\Http\Request;
 class DemoController extends Controller
 {
     function DemoAction(Request $request) {
-        if($request->accepts(['text/html'])) {
-            return true;
-        }else {
-            return false;
-        }
+        return $request->cookie();
     }
 }
