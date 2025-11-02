5b8a43c6b8d338ccf339f610f4d36c56c0f6026c
nmsaikat <nmsaikat.cf@gmail.com>
2025-11-03 04:01:01 +0600

    Laravel Different Response JSON

--- r-app/app/Http/Controllers/DemoController.php
+++ r-app/app/Http/Controllers/DemoController.php
@@ -2,22 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class DemoController extends Controller
 {
-    function DemoAction(Request $request) {
-        return [
-            "city" => "New York",
-            "temperature" => "22°C",
-            "condition" => "Sunny",
-            "hourly_forecast" => [
-                ["time" => "1 PM", "temp" => "21°C", "condition" => "Sunny"],
-                ["time" => "2 PM", "temp" => "22°C", "condition" => "Sunny"],
-                ["time" => "3 PM", "temp" => "23°C", "condition" => "Partly Cloudy"],
-                ["time" => "4 PM", "temp" => "22°C", "condition" => "Partly Cloudy"],
-                ["time" => "5 PM", "temp" => "21°C", "condition" => "Cloudy"],
-            ]
+    function DemoAction(Request $request):JsonResponse {
+
+        $code = 201;
+        $content = [
+            "name" => "Saikat",
+            "city" => "Dhaka",
+            "country" => "Bangladesh",
         ];
+        return response()->json($content, $code);
     }
 }
