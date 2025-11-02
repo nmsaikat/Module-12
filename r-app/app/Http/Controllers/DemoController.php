b2c30ca333c4427db7d305a5db002c66efa06cd7
nmsaikat <nmsaikat.cf@gmail.com>
2025-11-03 03:47:22 +0600

    Different Response Format

--- r-app/app/Http/Controllers/DemoController.php
+++ r-app/app/Http/Controllers/DemoController.php
@@ -7,6 +7,17 @@ use Illuminate\Http\Request;
 class DemoController extends Controller
 {
     function DemoAction(Request $request) {
-        return $request->cookie();
+        return [
+            "city" => "New York",
+            "temperature" => "22°C",
+            "condition" => "Sunny",
+            "hourly_forecast" => [
+                ["time" => "1 PM", "temp" => "21°C", "condition" => "Sunny"],
+                ["time" => "2 PM", "temp" => "22°C", "condition" => "Sunny"],
+                ["time" => "3 PM", "temp" => "23°C", "condition" => "Partly Cloudy"],
+                ["time" => "4 PM", "temp" => "22°C", "condition" => "Partly Cloudy"],
+                ["time" => "5 PM", "temp" => "21°C", "condition" => "Cloudy"],
+            ]
+        ];
     }
 }
